4686c61120705f1fa8d61c4ec62997551d43adab
priyanshu-Maniyar <147547319+priyanshu-Maniyar@users.noreply.github.com>
2024-07-22 23:49:49 +0530

    Update user_ops.c

--- user_ops.c
+++ user_ops.c
@@ -1,275 +1,216 @@
-#include"declarations.h"
-#include<stdio.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<stdlib.h>
-#include<netinet/in.h>
-#include<string.h>
-#include<pthread.h>
-#include<sys/ipc.h>
-#include<sys/shm.h>
-#include<sys/sem.h>
+#include "declarations.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sqlite3.h>
 
-extern char** userIdTable[3];
+extern sqlite3 *db;
 
-// ------ Search related operations --------------
-long int balanceEnquiry(int id, int type){
-  long int bal = -1;
-  if(type==NORMAL_USER){
-    struct Data dt = searchNorm(id);
-    bal = dt.balance;
-  }
-  else if(type==JOINT_USER){
-    struct JointData jdt = searchJoint(id);
-    bal = jdt.balance;
-  }
-
-  return bal;
+// Helper function to execute a SQL query
+int executeSQL(const char *sql, sqlite3_stmt **stmt) {
+    int rc = sqlite3_prepare_v2(db, sql, -1, stmt, NULL);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "Failed to execute SQL query: %s\n", sqlite3_errmsg(db));
+        return -1;
+    }
+    return 0;
 }
 
+// ---------- Search related operations --------------
+long int balanceEnquiry(int id, int type) {
+    long int bal = -1;
+    sqlite3_stmt *stmt;
+    const char *sql;
+    
+    if (type == NORMAL_USER) {
+        sql = "SELECT Balance FROM NormalUser WHERE Id = ?";
+        if (executeSQL(sql, &stmt) != 0) return -1;
+        sqlite3_bind_int(stmt, 1, id);
+        if (sqlite3_step(stmt) == SQLITE_ROW) {
+            bal = sqlite3_column_int(stmt, 0);
+        }
+        sqlite3_finalize(stmt);
+    } else if (type == JOINT_USER) {
+        sql = "SELECT Balance FROM JointUser WHERE Id = ?";
+        if (executeSQL(sql, &stmt) != 0) return -1;
+        sqlite3_bind_int(stmt, 1, id);
+        if (sqlite3_step(stmt) == SQLITE_ROW) {
+            bal = sqlite3_column_int(stmt, 0);
+        }
+        sqlite3_finalize(stmt);
+    }
 
-struct Data viewDetailsNorm(int id){
-  return searchNorm(id);
+    return bal;
 }
 
-struct JointData viewDetailsJoint(int id){
-  // search ops will take care of synchronizations if needed
-  return searchJoint(id);
+struct Data viewDetailsNorm(int id) {
+    struct Data dt;
+    sqlite3_stmt *stmt;
+    const char *sql = "SELECT Username, Password, Balance FROM NormalUser WHERE Id = ?";
+
+    if (executeSQL(sql, &stmt) != 0) return dt;
+
+    sqlite3_bind_int(stmt, 1, id);
+    if (sqlite3_step(stmt) == SQLITE_ROW) {
+        strncpy(dt.user.username, (const char *)sqlite3_column_text(stmt, 0), MAX_CHAR_LEN - 1);
+        strncpy(dt.user.password, (const char *)sqlite3_column_text(stmt, 1), MAX_CHAR_LEN - 1);
+        dt.balance = sqlite3_column_int(stmt, 2);
+        dt.id = id;
+    }
+    sqlite3_finalize(stmt);
+
+    return dt;
+}
+
+struct JointData viewDetailsJoint(int id) {
+    struct JointData jdt;
+    sqlite3_stmt *stmt;
+    const char *sql = "SELECT Keyword, Username1, Password1, Username2, Password2, Balance FROM JointUser WHERE Id = ?";
+
+    if (executeSQL(sql, &stmt) != 0) return jdt;
+
+    sqlite3_bind_int(stmt, 1, id);
+    if (sqlite3_step(stmt) == SQLITE_ROW) {
+        strncpy(jdt.keyword, (const char *)sqlite3_column_text(stmt, 0), MAX_CHAR_LEN - 1);
+        strncpy(jdt.user1.username, (const char *)sqlite3_column_text(stmt, 1), MAX_CHAR_LEN - 1);
+        strncpy(jdt.user1.password, (const char *)sqlite3_column_text(stmt, 2), MAX_CHAR_LEN - 1);
+        strncpy(jdt.user2.username, (const char *)sqlite3_column_text(stmt, 3), MAX_CHAR_LEN - 1);
+        strncpy(jdt.user2.password, (const char *)sqlite3_column_text(stmt, 4), MAX_CHAR_LEN - 1);
+        jdt.balance = sqlite3_column_int(stmt, 5);
+        jdt.id = id;
+    }
+    sqlite3_finalize(stmt);
+
+    return jdt;
 }
 
 // ---------- Modify related operations -----------
-int passwordChangeNorm(int id, char* password){
-  // 4th semaphore (exclusive to modification operation)
-  int key = ftok(".", 'a');
-  int semid = semget(key, 0, 0);
-  struct sembuf semlock = {4, -1, SEM_UNDO}; // 4th semaphore
-  semop(semid, &semlock, 1);
+int passwordChangeNorm(int id, char *password) {
+    sqlite3_stmt *stmt;
+    const char *sql = "UPDATE NormalUser SET Password = ? WHERE Id = ?";
 
-  struct Data dt = searchNorm(id);
-  strncpy(dt.user.password, password, MAX_CHAR_LEN-1); // only password is changed
+    if (executeSQL(sql, &stmt) != 0) return -1;
 
+    sqlite3_bind_text(stmt, 1, password, -1, SQLITE_STATIC);
+    sqlite3_bind_int(stmt, 2, id);
 
-  // write lock the database record space
-  int norm_fd = open("Normal_account.txt", O_RDWR);
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        fprintf(stderr, "Failed to update password: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(stmt);
+        return -1;
+    }
+    sqlite3_finalize(stmt);
 
-  struct flock recordlock;
-  recordlock.l_type = F_WRLCK;
-  recordlock.l_whence = SEEK_SET;
-  recordlock.l_start = id*sizeof(struct Data);
-  recordlock.l_len = sizeof(struct Data);
-  recordlock.l_pid = getpid();
-  fcntl(norm_fd, F_SETLKW, &recordlock);
-
-  lseek(norm_fd, recordlock.l_start, SEEK_SET);
-
-  write(norm_fd, &dt, sizeof(struct Data));
-
-  // unlock the database record space
-  recordlock.l_type = F_UNLCK;
-  fcntl(norm_fd, F_SETLK, &recordlock);
-
-  // unlock 4th semaphore (exclusive to modification operation)
-  semlock.sem_op = 1;
-  semop(semid, &semlock, 1);
-
-  return 0;
+    return 0;
 }
 
-int passwordChangeJoint(int id, char* password, char* cur_username){
+int passwordChangeJoint(int id, char *password, char *cur_username) {
+    sqlite3_stmt *stmt;
+    const char *sql = "UPDATE JointUser SET Password1 = CASE WHEN Username1 = ? THEN ? ELSE Password1 END, "
+                      "Password2 = CASE WHEN Username2 = ? THEN ? ELSE Password2 END "
+                      "WHERE Id = ?";
 
-  // 4th semaphore (exclusive to modification operation)
-  int key = ftok(".", 'a');
-  int semid = semget(key, 0, 0);
-  struct sembuf semlock = {4, -1, SEM_UNDO}; // 4th semaphore
-  semop(semid, &semlock, 1);
+    if (executeSQL(sql, &stmt) != 0) return -1;
 
-  struct JointData jdt = searchJoint(id);
-  if(!strcmp(jdt.user1.username, cur_username)){
-    strncpy(jdt.user1.password, password, MAX_CHAR_LEN-1);
-  }
-  else if(!strcmp(jdt.user2.username, cur_username)){
-    strncpy(jdt.user2.password, password, MAX_CHAR_LEN-1);
-  }
+    sqlite3_bind_text(stmt, 1, cur_username, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, password, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 3, cur_username, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 4, password, -1, SQLITE_STATIC);
+    sqlite3_bind_int(stmt, 5, id);
 
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        fprintf(stderr, "Failed to update joint password: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(stmt);
+        return -1;
+    }
+    sqlite3_finalize(stmt);
 
-  // write lock the database record space
-  int joint_fd = open("Joint_account.txt", O_RDWR);
-
-  struct flock recordlock;
-  recordlock.l_type = F_WRLCK;
-  recordlock.l_whence = SEEK_SET;
-  recordlock.l_start = id*sizeof(struct JointData);
-  recordlock.l_len = sizeof(struct JointData);
-  recordlock.l_pid = getpid();
-  fcntl(joint_fd, F_SETLKW, &recordlock);
-
-  lseek(joint_fd, recordlock.l_start, SEEK_SET);
-
-  write(joint_fd, &jdt, sizeof(struct JointData));
-
-  // unlock the database record space
-  recordlock.l_type = F_UNLCK;
-  fcntl(joint_fd, F_SETLK, &recordlock);
-
-  // unlock 4th semaphore (exclusive to modification operation)
-  semlock.sem_op = 1;
-  semop(semid, &semlock, 1);
-
-  return 0;
+    return 0;
 }
 
-int depositAmtNorm(int id, long int amt){
-  // 4th semaphore (exclusive to modification operation)
-  int key = ftok(".", 'a');
-  int semid = semget(key, 0, 0);
-  struct sembuf semlock = {4, -1, SEM_UNDO}; // 4th semaphore
-  semop(semid, &semlock, 1);
+int depositAmtNorm(int id, long int amt) {
+    sqlite3_stmt *stmt;
+    const char *sql = "UPDATE NormalUser SET Balance = Balance + ? WHERE Id = ?";
 
-  struct Data dt = searchNorm(id);
-  dt.balance += amt; // balance is increased by amt
+    if (executeSQL(sql, &stmt) != 0) return -1;
 
-  // write lock the database record space
-  int norm_fd = open("Normal_account.txt", O_RDWR);
+    sqlite3_bind_int(stmt, 1, amt);
+    sqlite3_bind_int(stmt, 2, id);
 
-  struct flock recordlock;
-  recordlock.l_type = F_WRLCK;
-  recordlock.l_whence = SEEK_SET;
-  recordlock.l_start = id*sizeof(struct Data);
-  recordlock.l_len = sizeof(struct Data);
-  recordlock.l_pid = getpid();
-  fcntl(norm_fd, F_SETLKW, &recordlock);
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        fprintf(stderr, "Failed to deposit amount: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(stmt);
+        return -1;
+    }
+    sqlite3_finalize(stmt);
 
-  lseek(norm_fd, recordlock.l_start, SEEK_SET);
-
-  write(norm_fd, &dt, sizeof(struct Data));
-
-  // unlock the database record space
-  recordlock.l_type = F_UNLCK;
-  fcntl(norm_fd, F_SETLK, &recordlock);
-
-  // unlock 4th semaphore (exclusive to modification operation)
-  semlock.sem_op = 1;
-  semop(semid, &semlock, 1);
-
-  return 0;
+    return 0;
 }
 
-int depositAmtJoint(int id, long int amt){
+int depositAmtJoint(int id, long int amt) {
+    sqlite3_stmt *stmt;
+    const char *sql = "UPDATE JointUser SET Balance = Balance + ? WHERE Id = ?";
 
-  // 4th semaphore (exclusive to modification operation)
-  int key = ftok(".", 'a');
-  int semid = semget(key, 0, 0);
-  struct sembuf semlock = {4, -1, SEM_UNDO}; // 4th semaphore
-  semop(semid, &semlock, 1);
+    if (executeSQL(sql, &stmt) != 0) return -1;
 
-  struct JointData jdt = searchJoint(id);
-  jdt.balance += amt; // only balance is increased
+    sqlite3_bind_int(stmt, 1, amt);
+    sqlite3_bind_int(stmt, 2, id);
 
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        fprintf(stderr, "Failed to deposit joint amount: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(stmt);
+        return -1;
+    }
+    sqlite3_finalize(stmt);
 
-  // write lock the database record space
-  int joint_fd = open("Joint_account.txt", O_RDWR);
-
-  struct flock recordlock;
-  recordlock.l_type = F_WRLCK;
-  recordlock.l_whence = SEEK_SET;
-  recordlock.l_start = id*sizeof(struct JointData);
-  recordlock.l_len = sizeof(struct JointData);
-  recordlock.l_pid = getpid();
-  fcntl(joint_fd, F_SETLKW, &recordlock);
-
-  lseek(joint_fd, recordlock.l_start, SEEK_SET);
-
-  write(joint_fd, &jdt, sizeof(struct JointData));
-
-  // unlock the database record space
-  recordlock.l_type = F_UNLCK;
-  fcntl(joint_fd, F_SETLK, &recordlock);
-
-  // unlock 4th semaphore (exclusive to modification operation)
-  semlock.sem_op = 1;
-  semop(semid, &semlock, 1);
-
-  return 0;
+    return 0;
 }
 
-int withdrawAmtNorm(int id, long int amt){
-  // 4th semaphore (exclusive to modification operation)
-  int key = ftok(".", 'a');
-  int semid = semget(key, 0, 0);
-  struct sembuf semlock = {4, -1, SEM_UNDO}; // 4th semaphore
-  semop(semid, &semlock, 1);
+int withdrawAmtNorm(int id, long int amt) {
+    sqlite3_stmt *stmt;
+    const char *sql = "UPDATE NormalUser SET Balance = Balance - ? WHERE Id = ? AND Balance >= ?";
 
-  struct Data dt = searchNorm(id);
-  if(dt.balance-amt<0)
-    return -1; // error in withdrawal process. Not sufficient amt in the a/c
-  dt.balance -= amt; // balance is decreased by amt
+    if (executeSQL(sql, &stmt) != 0) return -1;
 
+    sqlite3_bind_int(stmt, 1, amt);
+    sqlite3_bind_int(stmt, 2, id);
+    sqlite3_bind_int(stmt, 3, amt);
 
-  // write lock the database record space
-  int norm_fd = open("Normal_account.txt", O_RDWR);
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        fprintf(stderr, "Failed to withdraw amount: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(stmt);
+        return -1;
+    }
+    sqlite3_finalize(stmt);
 
-  struct flock recordlock;
-  recordlock.l_type = F_WRLCK;
-  recordlock.l_whence = SEEK_SET;
-  recordlock.l_start = id*sizeof(struct Data);
-  recordlock.l_len = sizeof(struct Data);
-  recordlock.l_pid = getpid();
-  fcntl(norm_fd, F_SETLKW, &recordlock);
-
-  lseek(norm_fd, recordlock.l_start, SEEK_SET);
-
-  write(norm_fd, &dt, sizeof(struct Data));
-
-  // unlock the database record space
-  recordlock.l_type = F_UNLCK;
-  fcntl(norm_fd, F_SETLK, &recordlock);
-
-  // unlock 4th semaphore (exclusive to modification operation)
-  semlock.sem_op = 1;
-  semop(semid, &semlock, 1);
-
-  return 0;
+    return 0;
 }
 
-int withdrawAmtJoint(int id, long int amt){
+int withdrawAmtJoint(int id, long int amt) {
+    sqlite3_stmt *stmt;
+    const char *sql = "UPDATE JointUser SET Balance = Balance - ? WHERE Id = ? AND Balance >= ?";
 
-  // 4th semaphore (exclusive to modification operation)
-  int key = ftok(".", 'a');
-  int semid = semget(key, 0, 0);
-  struct sembuf semlock = {4, -1, SEM_UNDO}; // 4th semaphore
-  semop(semid, &semlock, 1);
+    if (executeSQL(sql, &stmt) != 0) return -1;
 
-  struct JointData jdt = searchJoint(id);
-  if(jdt.balance - amt<0)
-    return -1;          // illegal withdrawal. Not sufficient amt in the a/c
+    sqlite3_bind_int(stmt, 1, amt);
+    sqlite3_bind_int(stmt, 2, id);
+    sqlite3_bind_int(stmt, 3, amt);
 
-  jdt.balance -= amt; // only balance is increased
+    if (sqlite3_step(stmt) != SQLITE_DONE) {
+        fprintf(stderr, "Failed to withdraw joint amount: %s\n", sqlite3_errmsg(db));
+        sqlite3_finalize(stmt);
+        return -1;
+    }
+    sqlite3_finalize(stmt);
 
-
-  // write lock the database record space
-  int joint_fd = open("Joint_account.txt", O_RDWR);
-
-  struct flock recordlock;
-  recordlock.l_type = F_WRLCK;
-  recordlock.l_whence = SEEK_SET;
-  recordlock.l_start = id*sizeof(struct JointData);
-  recordlock.l_len = sizeof(struct JointData);
-  recordlock.l_pid = getpid();
-  fcntl(joint_fd, F_SETLKW, &recordlock);
-
-  lseek(joint_fd, recordlock.l_start, SEEK_SET);
-
-  write(joint_fd, &jdt, sizeof(struct JointData));
-
-  // unlock the database record space
-  recordlock.l_type = F_UNLCK;
-  fcntl(joint_fd, F_SETLK, &recordlock);
-
-  // unlock 4th semaphore (exclusive to modification operation)
-  semlock.sem_op = 1;
-  semop(semid, &semlock, 1);
-
-  return 0;
+    return 0;
 }
